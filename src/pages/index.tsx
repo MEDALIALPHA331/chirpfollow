3e2fdf8671c552ff991ab211bf48cd34bcf7ae3e
Med_Ali_Khaled <72525144+MEDALIALPHA331@users.noreply.github.com>
2023-04-26 15:57:51 +0100

    updating schema, adding new post router, rendering posts in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data: postsData, isLoading } = api.posts.getAll.useQuery();
+
+  console.log(postsData);
 
   const user = useUser();
   //bg-gradient-to-b from-[#2e026d] to-[#15162c]
@@ -16,7 +18,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center ">
-        {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+        <div>{user.isSignedIn ? <SignOutButton /> : <SignInButton />}</div>
+
+        <div>
+          {postsData?.map((post) => {
+            return (
+              <div key={post.id}>
+                <span>{post.content}</span>
+                <span>By: {post.authorId}</span>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </>
   );
